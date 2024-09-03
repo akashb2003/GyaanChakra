a8f4623832c1764f875b725863bd87f05e1dfd9c
Akash Badadani <akashsgbadadani.com>
2024-09-03 13:18:27 +0530

    updated done

--- frontend/src/components/common/RightPanel.jsx
+++ frontend/src/components/common/RightPanel.jsx
@@ -30,7 +30,7 @@ const RightPanel = () => {
 	return (
 		<div className='hidden lg:block my-4 mx-2'>
 			<div className='bg-[#16181C] p-4 rounded-md sticky top-2'>
-				<p className='font-bold'>Who to follow</p>
+				<p className='font-bold'>Follow new People</p>
 				<div className='flex flex-col gap-4'>
 					{/* item */}
 					{isLoading && (
